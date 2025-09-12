707cf712fd2e23b1f5f90d8c2054a13b9bbebedd
Afdddd <kimdlsduq36@gmail.com>
2025-09-13 00:13:32 +0900

    init

---
+++ src/main/java/com/core/data_pipeline_platform/DataPipelinePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.core.data_pipeline_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPipelinePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPipelinePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/data_pipeline_platform/DataPipelinePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.data_pipeline_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPipelinePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
