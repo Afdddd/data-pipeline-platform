21170177b93cbf17e003217e15fc5cc6f8f877d1
Afdddd <kimdlsduq36@gmail.com>
2025-09-14 23:20:22 +0900

    fix: 파일 확장자 검증 로직 추가 및 파일 크기 검증 수정
    - validateFileExtension 메서드 추가: 파일 확장자 존재 여부 검증(controller 단에서 처리)
    - 파일 크기 검증 로직에서 DataSize 사용으로 변경
    - application.yml에서 불필요한 app.file.max-size 설정 제거

--- src/main/java/com/core/data_pipeline_platform/domain/file/validator/FileValidator.java
+++ src/main/java/com/core/data_pipeline_platform/domain/file/validator/FileValidator.java
@@ -3,14 +3,15 @@ package com.core.data_pipeline_platform.domain.file.validator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class FileValidator {
-    
-    @Value("${app.file.max-size}")
-    private long maxFileSize;
+
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private DataSize maxFileSize;
     
     /**
      * 파일 업로드 기본 검증
@@ -21,6 +22,7 @@ public class FileValidator {
         validateFileExists(file);
         validateFileName(file);
         validateFileSize(file);
+        validateFileExtension(file);
     }
     
     /**
@@ -46,12 +48,23 @@ public class FileValidator {
      * 파일 크기 검증
      */
     private void validateFileSize(MultipartFile file) {
-        if (file.getSize() > maxFileSize) {
+        if (file.getSize() > maxFileSize.toBytes()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    String.format("파일 크기가 제한을 초과합니다. 최대: %s, 현재: %s",
-                    maxFileSize,
+                    String.format("파일 크기가 제한을 초과합니다. 최대: %s, 현재: %s바이트",
+                    maxFileSize.toMegabytes() + "MB",
                     file.getSize())
             );
+        }
+    }
+
+    /**
+     * 파일 확장자 검증
+     */
+    private void validateFileExtension(MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        int idx = fileName.lastIndexOf('.');
+        if(idx == -1){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "확장자가 없습니다.");
         }
     }
 }
