0acc9991c46e6f3d7e675704a93bfef3b5046e5d
Afdddd <kimdlsduq36@gmail.com>
2025-10-26 23:06:53 +0900

    feat: 청크 업로드 DTO에 빌더 추가
    - ChunkUploadStartRequest에 @Builder 추가
    - ChunkUploadRequest에 @Builder 추가

--- src/main/java/com/core/data_pipeline_platform/domain/file/dto/ChunkUploadStartRequest.java
+++ src/main/java/com/core/data_pipeline_platform/domain/file/dto/ChunkUploadStartRequest.java
@@ -3,7 +3,9 @@ package com.core.data_pipeline_platform.domain.file.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 
+@Builder
 public record ChunkUploadStartRequest(
         @NotBlank String fileName,
         @Positive long totalSize,
