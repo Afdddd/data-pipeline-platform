ae4e1b2e0340b74abe3d5d1adcfd5acac8424940
Afdddd <kimdlsduq36@gmail.com>
2025-12-04 23:58:38 +0900

    migrate(model): SensorData를 Kotlin 데이터 클래스로 변환

--- src/main/java/com/core/data_pipeline_platform/domain/generator/model/SensorData.java
+++ src/main/java/com/core/data_pipeline_platform/domain/generator/model/SensorData.java
@@ -1,13 +1,8 @@
 package com.core.data_pipeline_platform.domain.generator.model;
 
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class SensorData {
-    private String sensorId;
-    private double value;
-    private String timestamp;
-    private String status;
-}
+data class SensorData (
+    val sensorId: String
+    val value: Double
+    val timestamp:  String
+    val status: String
+)
